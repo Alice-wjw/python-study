9e5f05205540834d224648d3ece97cc03af88534
Alice-wjw <wu5158123@gmail.com>
2026-08-13 23:14:49 +0800

    sixthday

---
+++ python数值分析/pandas/exercise1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+vall=[10,20,30,40,50]
+s1=pd.Series(vall)
+print(s1)
+print(type(s1))
+val2=['苹果','香蕉','橙子','葡萄']
+index2=['水果1','水果2','水果3','水果4']
+s2=pd.Series(val2,index=index2)
+print(s2)
+dic1={'姓名':'张三','年龄':20,'性别':'男','成绩':95}
+s3=pd.Series(dic1)
+print(s3)
+print(s2.index)
+print(type(s2.index))
+print(s2.values)
+print(type(s2.values))
+data1=[['张三',20,'男',98],['李四',19,'女',92]]
+d=['姓名','年龄','性别','成绩']
+c=pd.DataFrame(data1,columns=d)
+print(c)
